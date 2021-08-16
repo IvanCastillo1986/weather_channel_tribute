0d5696a4e1ff97e6339920db3b9f467332040709
Ivan Castillo <total180training@gmail.com>
2021-08-16 00:18:19 -0400

    Added rest of CurrentWeather card

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -4,22 +4,26 @@ import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 
 export default function WeatherCard({ weatherData }) {
 
-    // const currentConditions = weatherData.currentConditions
+    const currentConditions = weatherData.currentConditions
 
     return (
         <Card style={{width: '15rem'}}>
             <Card.Body>
-                <Card.Title>Today</Card.Title>
+                <Card.Title style={{fontWeight: 'bold'}}>Current Forecast</Card.Title>
                 <Card.Text>
                     {weatherData.description}
                 </Card.Text>
+                {
+                weatherData.currentConditions ?
                 <ListGroup>
-                    <ListGroupItem>Temperature: </ListGroupItem>
-                    <ListGroupItem>Feels Like: </ListGroupItem>
-                    <ListGroupItem>Humidity: </ListGroupItem>
-                    <ListGroupItem>Windspeed: </ListGroupItem>
-                    <ListGroupItem>Conditions: </ListGroupItem>
-                </ListGroup>
+                    <ListGroupItem>Temperature: {currentConditions.temp}</ListGroupItem>
+                    <ListGroupItem>Feels Like: {currentConditions.feelslike}</ListGroupItem>
+                    <ListGroupItem>Humidity: {currentConditions.humidity}</ListGroupItem>
+                    <ListGroupItem>Windspeed: {currentConditions.windspeed}</ListGroupItem>
+                    <ListGroupItem>Conditions: {currentConditions.conditions}</ListGroupItem>
+                </ListGroup> :
+                null
+                }
             </Card.Body>
         </Card>
     )
