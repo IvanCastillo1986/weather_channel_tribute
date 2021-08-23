91f3b5b7d75e7370a9de5093a9ce74e5fcd4b080
Ivan Castillo <total180training@gmail.com>
2021-08-23 13:40:06 -0400

    Rendering forecast for each day in ThreeDayWeather

--- src/Components/ThreeDayWeather.js
+++ src/Components/ThreeDayWeather.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import WeatherCard from './WeatherCard'
 
 
 
@@ -11,11 +12,19 @@ export default function ThreeDayWeather() {
         axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Queens%2C%20NY/next2days?unitGroup=us&key=3LBCQN3X7PM6J5L92ZBUUJEZ8`)
         .then(res => setWeather(res.data))
     }, [])
+    console.log(weather.days)
 
 
     return (
         <div>
             <h1>Three Day Weather</h1>
+            { 
+            weather.days ?
+            weather.days.map((day, i) => {
+                return <WeatherCard key={i} day={day} />
+            }) 
+            : null 
+            }
         </div>
     )
 }
--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 
 
-export default function WeatherCard({ weatherData }) {
+export default function WeatherCard({ day }) {
 
-    const currentConditions = weatherData.currentConditions
+    console.log(day)
 
     return (
         <Card style={{width: '15rem'}}>
             <Card.Body>
                 <Card.Title style={{fontWeight: 'bold'}}>Current Forecast</Card.Title>
                 <Card.Text>
-                    {weatherData.description}
+                    {day.description}
                 </Card.Text>
                 {
-                weatherData.currentConditions ?
+                day.conditions ?
                 <ListGroup>
-                    <ListGroupItem>Temperature: {currentConditions.temp}</ListGroupItem>
-                    <ListGroupItem>Feels Like: {currentConditions.feelslike}</ListGroupItem>
-                    <ListGroupItem>Humidity: {currentConditions.humidity}</ListGroupItem>
-                    <ListGroupItem>Windspeed: {currentConditions.windspeed}</ListGroupItem>
-                    <ListGroupItem>Conditions: {currentConditions.conditions}</ListGroupItem>
+                    <ListGroupItem>Temperature: {day.temp}</ListGroupItem>
+                    <ListGroupItem>Feels Like: {day.feelslike}</ListGroupItem>
+                    <ListGroupItem>Humidity: {day.humidity}</ListGroupItem>
+                    <ListGroupItem>Windspeed: {day.windspeed}</ListGroupItem>
+                    <ListGroupItem>Conditions: {day.conditions}</ListGroupItem>
                 </ListGroup> :
                 null
                 }
