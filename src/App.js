76925f9f0384eb9fa759340545b746fee7d000d4
Ivan Castillo <total180training@gmail.com>
2021-10-06 23:59:50 -0400

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 
 import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
 import Logo from './Components/Logo'
 import DateTime from './Components/DateTime'
 import Home from './Pages/Home'
@@ -15,22 +16,27 @@ export default function App() {
 
 
   return (
-    <div className='App'>
-      <NavBar />
-      <main>
-        <Logo />
-        <header className='Header'>
-          <span className='title'>Latest Observations</span>
-          <DateTime />
-        </header>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/current' component={Current} />
-          <Route path='/3day' component={ThreeDay} />
-          <Route path='/about' component={About} />
-        </Switch>
-      </main>
-    </div>
+    // <div className='AppContainer'>
+
+      <div className='App'>
+        <NavBar />
+        <main>
+          <Logo />
+          <header className='Header'>
+            <span className='title'>Latest Observations</span>
+            <DateTime />
+          </header>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/current' component={Current} />
+            <Route path='/3day' component={ThreeDay} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </main>
+      <Footer />
+      </div>
+      
+    // </div>
   )
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import '../App.css'
+
+
+export default function Footer() {
+
+    
+
+    return (
+        <div className='Footer'>
+            <h1>Barometric Pressure: 29.87 R</h1>
+        </div>
+    )
+}
