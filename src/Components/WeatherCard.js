756036591d058ba8956d5c6609d73f5392b8f2a1
Ivan Castillo <total180training@gmail.com>
2021-10-07 23:59:09 -0400

    Added some content to .WeatherCard

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -15,14 +15,13 @@ export default function WeatherCard({ day }) {
                     {day.description}
                 </p>
                 {
-                day.conditions ?
-                <ul>
-                    <li>Temperature: {day.temp}</li>
-                    <li>Feels Like: {day.feelslike}</li>
-                    <li>Humidity: {day.humidity}</li>
-                    <li>Windspeed: {day.windspeed}</li>
-                    <li>Conditions: {day.conditions}</li>
-                </ul> :
+                day.conditions 
+                ?
+                <div className='LoHiDiv'>
+                    <div><span>Lo</span><span>34</span></div> 
+                    <div><span>Hi</span><span>44</span></div> 
+                </div>
+                :
                 null
                 }
         </div>
